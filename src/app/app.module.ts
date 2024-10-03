9e721f8b3b67ab183a07588f7f20a103021f561e
Suresh Nayak <suresh.nayak@acheron-tech.com>
2024-10-03 18:26:26 +0530

    Branding changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
+import { HomeViewDetialsComponent } from "./home/home-view-detials/home-view-detials.component";
+import { MaterialModule } from './core/material/material.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         FormsModule,
         BrowserAnimationsModule,
-        CoreModule
+        CoreModule,
+        HomeViewDetialsComponent,
+        MaterialModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
